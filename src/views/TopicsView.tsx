cc5f0b8b24259b4ba701bf7fdaa2a754de5fd5f8
Spencer M <spencer.g.moffat@gmail.com>
2024-03-16 09:46:27 -0600

    Add topics tabbed display

--- src/views/TopicsView.tsx
+++ src/views/TopicsView.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+
+import { Box, Tabs, Title } from "@mantine/core";
+
+import { CourseCardsDisplay } from "../components";
 import { Course } from "../types/Course";
 import { Topic } from "../types/Topic";
 
@@ -7,12 +12,63 @@ type Props = {
 };
 
 const TopicsView: React.FC<Props> = ({ courses, topics }) => {
-  console.log(courses);
-  console.log(topics);
+  const [activeTab, setActiveTab] = useState<string | null>(
+    topics.length !== 0 ? topics[0].id : null
+  );
+
   return (
-    <div>
-      <h1>Topics View</h1>
-    </div>
+    <>
+      <Title order={1} pb="md">
+        Topics
+      </Title>
+      {topics.length !== 0 ? (
+        <Tabs
+          value={activeTab}
+          onChange={setActiveTab}
+          variant="default"
+          orientation="vertical"
+          defaultValue={topics[0].id}
+        >
+          <Tabs.List>
+            {topics.map((topic) => {
+              return (
+                <Tabs.Tab
+                  key={topic.id}
+                  value={topic.id}
+                  styles={{
+                    tabLabel:
+                      activeTab !== null && topic.id == activeTab
+                        ? { fontWeight: "700" }
+                        : {},
+                  }}
+                >
+                  {topic.name}
+                </Tabs.Tab>
+              );
+            })}
+          </Tabs.List>
+          {topics.map((topic) => {
+            return (
+              <Tabs.Panel key={topic.id} value={topic.id}>
+                <Box px="md">
+                  <Title order={3} pb="md">
+                    {topic.name}
+                  </Title>
+                  <CourseCardsDisplay
+                    courses={courses.filter((c) =>
+                      topic.courseGroupIds.includes(c.courseGroupId)
+                    )}
+                    justify="flex-start"
+                  />
+                </Box>
+              </Tabs.Panel>
+            );
+          })}
+        </Tabs>
+      ) : (
+        <p>No topics found.</p>
+      )}
+    </>
   );
 };
 
